8b4d276ea0ca518d56a90d95d67e782a328bcebd
Veysel559 <veyselmaras2@gmail.com>
2023-07-06 17:58:23 +0300

    day3

--- app.js
+++ app.js
@@ -28,7 +28,15 @@ app.get("/contact", function (req, res) {
 app.get("/about", function (req, res) {
   res.render("about", { aboutContent: aboutContent });
 });
-
+app.get("/compose", function (req, res) {
+  res.render("compose");
+});
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+});
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
