362db2ae2be2bf21fe97e2e57d3f816207d2ea6c
Veysel559 <veyselmaras2@gmail.com>
2023-07-10 18:10:39 +0300

    lesson

--- app.js
+++ app.js
@@ -41,6 +41,19 @@ app.post("/compose", function (req, res) {
   posts.push(post);
   res.redirect("/");
 });
+
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = req.params.postName;
+
+  posts.forEach(function (post) {
+    const storedTitle = post.title;
+
+    if (storedTitle === requestedTitle) {
+      console.log("Match found!");
+    }
+  });
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
